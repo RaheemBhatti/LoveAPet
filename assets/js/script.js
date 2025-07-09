dc54442915fb3bb4de552cdded47a90ae694c025
Abdul Raheem <92326553+RaheemBhatti@users.noreply.github.com>
2025-07-09 05:11:21 +0500

    Updated

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".cardBxwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    240: {
+      slidesPerView: 1.3,
+      spaceBetween: 20,
+      centeredSlides: true,
+      loop: true,
+    },
+    550: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
